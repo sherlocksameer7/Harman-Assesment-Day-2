9b8c680d64f40e2fb84f91018f9eecbada06de4e
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-04 08:51:07 +0530

    Correction of Checking an Integer or not

--- checking_integer.py
+++ checking_integer.py
@@ -1,5 +1,11 @@
-a = (input("Enter anything like number or words "))
-if isinstance(a, int):
+# a = (input("Enter anything like number or words "))
+# if isinstance(a, int):
+#     print("It is an Integer")
+# else:
+#     print("It is not an Integer")
+
+inp = input("Enter any input: ")
+if inp.isdigit():
     print("It is an Integer")
 else:
     print("It is not an Integer")
